4fb9bfab77432e781a63c4a31d84c9586603dd6f
AugustinCablant <123320681+AugustinCablant@users.noreply.github.com>
2023-11-16 17:06:49 +0100

    Update scrap_iso_code.py

---
+++ Notebooks/scrap_iso_code.py
@@ -0,0 +1,6 @@
+######################### We want to scrap the ISO CODE of all countries in the world #########################
+# We use WIKIPEDIA
+URL = 'https://en.wikipedia.org/wiki/List_of_ISO_3166_country_codes'
+
+# Some imports
+import 
