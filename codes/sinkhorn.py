5c083ee5789e7a54ca8fae973d8bae81b9a7f17a
AugustinCablant <123320681+AugustinCablant@users.noreply.github.com>
2024-01-21 22:17:03 +0100

    Update sinkhorn.py

--- codes/sinkhorn.py
+++ codes/sinkhorn.py
@@ -40,6 +40,52 @@ def sinkhorn(a, b, C, epsilon = 1e-3, max_iters = 10000):
             return T 
     return False
 
-def algo1(u_0, v_0, W_0): 
+def algo1(u_0, v_0, W_0, delta_u2, delta_v2, delta_W2, max_iters = 1000): 
     "Algorithm 1 Random walk Metropolis within Gibbs"
+    
+    #First function to check if (x,y,Z) is in U
+    def is_in_U(x,y,Z):
+        if 0 <= x <= 1:
+            if 0 <= y <= 1:
+                if 0 <= Z <= 1: return True
+                else: return False
+            else: return None
+        else: return None
+    #Second function to compute the probability a((u,v,W),(x,y,Z))
+    def a(u,v,W,x,y,Z):
+        return None
+    
+    ## 
+    u = [u_0]
+    v = [v_0] 
+    W = [W_0]
+    for _ in range(max_iters):
+        #u
+        x = u[-1] + np.random.normal(0, np.sqrt(delta_u2))
+        y = v[-1] 
+        Z = W[-1] 
+        if is_in_U(x,y,Z):
+            if probability: 
+                u.append(x)
+                v.append(y)
+                W.append(Z)
+        #v
+        x = u[-1] 
+        y = v[-1] + np.random.normal(0, np.sqrt(delta_v2))
+        Z = W[-1] 
+        if is_in_U(x,y,Z):
+            if probability: 
+                u.append(x)
+                v.append(y)
+                W.append(Z)
+        #W 
+        x = u[-1] 
+        y = v[-1] 
+        Z = W[-1] + np.random.normal(0, np.sqrt(delta_W2))
+        if is_in_U(x,y,Z):
+            if probability: 
+                u.append(x)
+                v.append(y)
+                W.append(Z)
+
     
